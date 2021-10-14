d05b729f1ab05d32acb562b9c691ff295291df4d
hkjj293 <chrishuihuihui@gmail.com>
2021-10-14 22:06:05 +0100

    updated l11 bsttemplate

--- BSTTemplate.h
+++ BSTTemplate.h
@@ -1,20 +1,114 @@
 #pragma once
 #include <string>
+#include <iostream>
 
+template <typename T>
 struct node {
-	int value;
+	T value;
 	struct node* left;
 	struct node* right;
 };
 
-class BST {
+template <typename T>
+class BSTTemplate {
 public:
-	BST();
-	~BST();
-	void add_node(int val);
+	BSTTemplate();
+	~BSTTemplate();
+	void add_node(T val);
 	void print();
-	bool has_element(int val);
+	bool has_element(T val);
 private:
 	struct node* root;
 	int depth;
 };
+
+template <typename T>
+BSTTemplate<T>::BSTTemplate() {
+	root = NULL;
+	depth = 0;
+}
+
+template <typename T>
+BSTTemplate<T>::~BSTTemplate() {
+	struct node* curr = this->root;
+	struct node** stack = new node * [this->depth];
+	for (int i = 0; i < this->depth; i++) {
+		stack[i] = NULL;
+	}
+	int depth = 1;
+	bool checkLeft = true;
+	while (curr != NULL || depth != 1) {
+		while (curr != NULL) {
+			stack[depth - 1] = curr;
+			depth++;
+			curr = curr->left;
+		}
+		depth--;
+		curr = stack[depth - 1];
+		struct node* disposal = curr;
+		curr = curr->right;
+		std::cout << "deleting " << disposal->value << std::endl;
+		delete disposal;
+		disposal = NULL;
+		std::cout << "deleted" << std::endl;
+	}
+	delete[] stack;
+}
+
+template <typename T>
+void BSTTemplate<T>::add_node(T val) {
+	struct node** curr = &(this->root);
+	int depth = 1;
+	while ((*curr) != NULL) {
+		depth++;
+		if (val < (*curr)->value) {
+			curr = &((*curr)->left);
+		}
+		else {
+			curr = &((*curr)->right);
+		}
+	}
+	*curr = new node;
+	(*curr)->left = NULL;
+	(*curr)->right = NULL;
+	(*curr)->value = val;
+	this->depth += (this->depth < depth) * (depth - this->depth);
+}
+
+template <typename T>
+void BSTTemplate<T>::print() {
+	struct node* curr = this->root;
+	struct node** stack = new node * [this->depth];
+	for (int i = 0; i < this->depth; i++) {
+		stack[i] = NULL;
+	}
+	int depth = 1;
+	bool checkLeft = true;
+	while (curr != NULL || depth != 1) {
+		while (curr != NULL) {
+			stack[depth - 1] = curr;
+			depth++;
+			curr = curr->left;
+		}
+		depth--;
+		curr = stack[depth - 1];
+		std::cout << curr->value << std::endl;
+		curr = curr->right;
+	}
+	delete[] stack;
+}
+
+template <typename T>
+bool BSTTemplate<T>::has_element(T val) {
+	struct node* curr = this->root;
+	while (curr != NULL) {
+		if (val == curr->value) return true;
+		if (val < curr->value) {
+			curr = curr->left;
+		}
+		else {
+			curr = curr->right;
+		}
+	}
+	return false;
+}
